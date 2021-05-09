dde966d5044506fc5d1083bf1bda26a365344f7b
Антон <write_me@live.ru>
2021-05-09 20:37:07 +0300

    +

--- TestK/src/Main.java
+++ TestK/src/Main.java
@@ -10,7 +10,6 @@ public class Main {
 
         listValuesTable.add(new ValuesTable("Природный газ ","*",2017,"Mtoe",  148.67));
 
-
         listMultiplicationTable.add(new MultiplicationTable("*","ft3ng","Gft3ng",-9));
         listMultiplicationTable.add(new MultiplicationTable("*","tce","Gtce",-9));
         listMultiplicationTable.add(new MultiplicationTable("*","toe","Gtoe",-9));
@@ -30,7 +29,6 @@ public class Main {
         listMultiplicationTable.add(new MultiplicationTable("*","ft3ng","Mft3ng",-6));
         listMultiplicationTable.add(new MultiplicationTable("*","wh","Gwh",-9));
 
-
         listConvertationTable.add(new ConvertationTable("*","Mtce","Mm3ng",751.4768963));
         listConvertationTable.add(new ConvertationTable("*","Gft3ng","Twh",0.301277062));
         listConvertationTable.add(new ConvertationTable("*","MMbtu","Mj",1055.060005));
@@ -39,9 +37,6 @@ public class Main {
         listConvertationTable.add(new ConvertationTable("*","Gj","Gwh",0.000277778));
         listConvertationTable.add(new ConvertationTable("*","Ktoe","Kboe",6.8419054));
         listConvertationTable.add(new ConvertationTable("*","Gm3ng","Gft3ng",35.958043));
-
-
-
 
         СonverterTest con =new СonverterTest(listMultiplicationTable,listConvertationTable);
         for (int i=0;i<12;i++){
